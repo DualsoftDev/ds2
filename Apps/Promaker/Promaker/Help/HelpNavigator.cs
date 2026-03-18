da2c1f348a02bc0e2610f83db9bdac6ddd11c32c
hskim <doya2524@dualsoft.com>
2026-03-18 15:39:06 +0900

    UI 디자인 변경 및 빌드 0.0.0.8

--- Apps/Promaker/Promaker/Help/HelpNavigator.cs
+++ Apps/Promaker/Promaker/Help/HelpNavigator.cs
@@ -1,4 +1,5 @@
-using System.Windows;
+using System.Collections.Generic;
+using System.Diagnostics;
 using CommunityToolkit.Mvvm.Input;
 using System.Windows.Input;
 
@@ -6,11 +7,34 @@ namespace Promaker.Help;
 
 public static class HelpNavigator
 {
+    private const string BaseUrl = "http://dualsoft.co.kr/ds2_manual";
+
+    private static readonly Dictionary<string, string> TopicUrls = new()
+    {
+        ["general"]                = $"{BaseUrl}/index.html",
+        ["file"]                   = $"{BaseUrl}/res/01_basic_modeling.html",
+        ["edit"]                   = $"{BaseUrl}/res/08_edit_menu.html",
+        ["simulation"]             = $"{BaseUrl}/res/03_simulation_and_tools.html",
+        ["process-control"]        = $"{BaseUrl}/res/03_simulation_and_tools.html",
+        ["explorer"]               = $"{BaseUrl}/res/04_explorer_panel.html",
+        ["history"]                = $"{BaseUrl}/res/04_explorer_panel.html",
+        ["properties"]             = $"{BaseUrl}/res/06_properties.html",
+        ["condition-auto-aux"]     = $"{BaseUrl}/res/02_conditions.html",
+        ["condition-com-aux"]      = $"{BaseUrl}/res/02_conditions.html",
+        ["condition-skip-unmatch"] = $"{BaseUrl}/res/02_conditions.html",
+        ["condition"]              = $"{BaseUrl}/res/02_conditions.html",
+        ["apicalls"]               = $"{BaseUrl}/res/07_apicalls.html",
+        ["tools"]                  = $"{BaseUrl}/res/05_csv_import.html",
+    };
+
     public static ICommand NavigateCommand { get; } = new RelayCommand<string?>(Navigate);
 
     public static void Navigate(string? topic)
     {
-        // TODO: 영상 준비 후 topic별 URL로 Process.Start
-        MessageBox.Show("준비 중입니다.", "도움말", MessageBoxButton.OK, MessageBoxImage.Information);
+        var url = topic is not null && TopicUrls.TryGetValue(topic, out var found)
+            ? found
+            : $"{BaseUrl}/index.html";
+
+        Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
     }
 }
