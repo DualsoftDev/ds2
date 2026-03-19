370cb8c69e4ccf65e36aee5eaaf52917b44dd1d2
dskim <dskim9752@dualsoft.com>
2026-03-19 12:10:59 +0900

    flow 선택 하이라이트 + 사이드 메뉴 휠클릭 구현 등, appsettings 최신 반영

--- Apps/DSPilot/DSPilot/Models/AppSettingsModel.cs
+++ Apps/DSPilot/DSPilot/Models/AppSettingsModel.cs
@@ -8,6 +8,8 @@ public class AppSettingsModel
     public DsPilotSettings DsPilot { get; set; } = new();
     public PlcDatabaseSettings PlcDatabase { get; set; } = new();
     public DspDatabaseSettings DspDatabase { get; set; } = new();
+    public PlcConnectionSettings PlcConnection { get; set; } = new();
+    public LoggingSettings Logging { get; set; } = new();
 }
 
 public class DsPilotSettings
@@ -34,6 +36,43 @@ public class DspDatabaseSettings
     public string Path { get; set; } = "%APPDATA%/DSPilot/dsp.db";
     public bool AutoCreate { get; set; } = true;
     public bool RecreateOnStartup { get; set; }
+
+    [JsonExtensionData]
+    public Dictionary<string, JsonElement>? ExtensionData { get; set; }
+}
+
+public class PlcConnectionSettings
+{
+    public bool Enabled { get; set; }
+    public string PlcName { get; set; } = "PLC_01";
+    public string IpAddress { get; set; } = "192.168.0.100";
+    public int ScanIntervalMs { get; set; } = 100;
+    public List<string> TagAddresses { get; set; } = new() { "Tag1", "Tag2", "Tag3" };
+
+    [JsonExtensionData]
+    public Dictionary<string, JsonElement>? ExtensionData { get; set; }
+}
+
+public class LoggingSettings
+{
+    public LogLevelSettings LogLevel { get; set; } = new();
+
+    [JsonExtensionData]
+    public Dictionary<string, JsonElement>? ExtensionData { get; set; }
+}
+
+public class LogLevelSettings
+{
+    public string Default { get; set; } = "Information";
+
+    [JsonPropertyName("Microsoft.AspNetCore")]
+    public string MicrosoftAspNetCore { get; set; } = "Warning";
+
+    [JsonPropertyName("DSPilot.Services")]
+    public string DsPilotServices { get; set; } = "Debug";
+
+    [JsonPropertyName("DSPilot.Repositories")]
+    public string DsPilotRepositories { get; set; } = "Debug";
 
     [JsonExtensionData]
     public Dictionary<string, JsonElement>? ExtensionData { get; set; }
