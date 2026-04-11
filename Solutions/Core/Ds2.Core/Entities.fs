b9b05c5048b486a618a5c2adce9b244ad6d92be9
승훈 안 <ahn@dualsoft.com>
2026-04-11 12:38:28 +0900

    ApiDefActionType 추가 및 IsPush 통합

--- Solutions/Core/Ds2.Core/Entities.fs
+++ Solutions/Core/Ds2.Core/Entities.fs
@@ -145,9 +145,16 @@ and CallCondition [<JsonConstructor>] internal () =
 and ApiDef [<JsonConstructor>] internal (name, parentId) =
     inherit DsChild(name, parentId)
 
-    [<AasxField("IsPush")>] member val IsPush : bool        = false with get, set
-    [<AasxField("TxGuid")>] member val TxGuid : Guid option = None  with get, set
-    [<AasxField("RxGuid")>] member val RxGuid : Guid option = None  with get, set
+    [<AasxField("ApiDefActionType")>] member val ApiDefActionType : ApiDefActionType = ApiDefActionType.Normal with get, set
+    [<AasxField("TxGuid")>]           member val TxGuid : Guid option = None  with get, set
+    [<AasxField("RxGuid")>]           member val RxGuid : Guid option = None  with get, set
+
+    /// IsPush 호환성 속성 (ApiDefActionType.Push로 통합됨)
+    [<System.Obsolete("IsPush is deprecated. Use ApiDefActionType instead.")>]
+    [<JsonIgnore>]
+    member this.IsPush
+        with get() = this.ApiDefActionType = ApiDefActionType.Push
+        and set(value) = if value then this.ApiDefActionType <- ApiDefActionType.Push else this.ApiDefActionType <- ApiDefActionType.Normal
 
     member this.DeepCopy() = DeepCopyHelper.jsonCloneEntity this
 
